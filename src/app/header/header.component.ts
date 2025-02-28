0cdd83ab3484a43470904c6c8b4ec7582ead15df
alerwann <>
2025-02-28 12:14:35 +0100

    maj accueil responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink,RouterLinkActive, MatButtonModule,MatMenuModule],
+  imports: [RouterLink,RouterLinkActive, MatButtonModule,MatMenuModule, MatToolbarModule, MatIconModule,MatSidenavModule, MatListModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
